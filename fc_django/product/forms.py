acf7ad2edd541778ae13fb3361c5548cf61207c7
ksi2564 <kimsungin5@gmail.com>
2022-03-23 15:24:24 +0900

    페이지 권한 설정 및 코드 리팩토링(forms.py에서 유효성검사만 수행)

--- fc_django/product/forms.py
+++ fc_django/product/forms.py
@@ -31,12 +31,3 @@ class RegisterForm(forms.Form):
         price = cleaned_data.get('price')
         description = cleaned_data.get('description')
         stock = cleaned_data.get('stock')
-
-        if name and price and description and stock:
-            product = Product(
-                name=name,
-                price=price,
-                description=description,
-                stock=stock,
-            )
-            product.save()
